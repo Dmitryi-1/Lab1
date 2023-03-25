09c5cd3e568fc96a460aad1f76eb58516e04db51
Dmitryi <127759789+Dmitryi-1@users.noreply.github.com>
2023-03-25 17:41:38 +0500

    1

--- src/main/java/ru/students/test_reset_service/controller/MyController.java
+++ src/main/java/ru/students/test_reset_service/controller/MyController.java
@@ -1,2 +1,31 @@
-package ru.students.test_reset_service.controller;public class MyController {
+package ru.students.test_reset_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.students.test_reset_service.model.Request;
+import ru.students.test_reset_service.model.Response;
+
+
+@RestController
+public class MyController
+{
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request)
+    {
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/ru/students/test_reset_service/model/Response.java
+++ src/main/java/ru/students/test_reset_service/model/Response.java
@@ -1,2 +1,16 @@
-package ru.students.test_reset_service.model;public class Response {
+package ru.students.test_reset_service.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Response
+{
+    private String uid;
+    private String operationUid;
+    private String systemTime;
+    private String code;
+    private String errorCode;
+    private String errorMessage;
 }
